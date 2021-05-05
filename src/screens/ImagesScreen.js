8d6f3c0efb995f05ebe5a2f1a0b1987849bf5a7c
Heorgii Bolotov <zhora2001zhora@gmail.com>
2021-05-05 13:03:14 +0300

    fetch once images from api

--- src/screens/ImagesScreen.js
+++ src/screens/ImagesScreen.js
@@ -1,7 +1,6 @@
 import { View, Text, StyleSheet, Alert, Dimensions } from "react-native";
-import React, {Component, useState} from "react";
-import { MenuButton, Logo } from "../components/header/header";
-// import ImagePicker from 'react-native-image-crop-picker'
+import React, {Component, useEffect, useState} from "react";
+import { ActivityIndicator } from 'react-native'
 import * as ImagePicker from 'expo-image-picker';
 import { TouchableWithoutFeedback, ScrollView } from 'react-native-gesture-handler'
 import { ImageComponent } from "../components/images/Img";
@@ -12,39 +11,69 @@ const first = (Dimensions.get('window').width / 5) * 3
 
 export default function ImagesScreen() {
     const [images, setImages] = useState({left: [], center: [], right: [], column: 'right'})
+    const [loading, setLoading] = useState(false)
 
-    const openImagePickerAsync = async () => {
-        let permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
-        if (permissionResult.granted === false) {
-            alert('Permission to access camera roll is required!');
-            return;
+    const fetchImages = _ => {
+        const getImages = async () => {
+            setLoading(true)
+            try {
+                // get books
+                const REQUEST = 'red+cars',
+                    COUNT = 21,
+                    API_KEY = '19193969-87191e5db266905fe8936d565'
+                const response = await fetch(`https://pixabay.com/api/?key=${API_KEY}&q=${REQUEST}&image_type=photo&per_page=${COUNT}`)
+                const { hits } = await response.json()
+                hits.map(image => {
+                    console.log(image)
+                    image = {localUri: image.userImageURL}
+                    setImages((prevImg) => {
+                        if (prevImg.column === 'right') {
+                            return {...prevImg, column: 'left', left: [...prevImg.left, image]}
+                        } else if ((prevImg.column === 'left' && (prevImg.left.length + prevImg.center.length + prevImg.right.length + 1) % 7 !== 2) || prevImg.column === 'center') {
+                            return {...prevImg, column: 'right', right: [...prevImg.right, image]}
+                        }
+                        return {...prevImg, column: 'center', center: [...prevImg.center, image]}
+                    })
+                })
+            } catch (err) {
+                console.log(err.message)
+            } finally {
+                setLoading(false)
+            }
         }
-        let pickerResult = await ImagePicker.launchImageLibraryAsync();
-        if (pickerResult.cancelled === true) {
-            return;
-        }
-        return { localUri: pickerResult.uri }
-    };
-
-    const pick = async _ => {
-        openImagePickerAsync().then((image) => {
-            // set images
-            setImages((prevImg) => {
-                if (prevImg.column === 'right') { return { ...prevImg, column: 'left', left: [...prevImg.left, image] }}
-                else if ((prevImg.column === 'left' && (prevImg.left.length + prevImg.center.length + prevImg.right.length + 1) % 7 !== 2) || prevImg.column === 'center') { return { ...prevImg, column: 'right', right: [...prevImg.right, image] } }
-                return { ...prevImg, column: 'center', center: [...prevImg.center, image] }
-            })
-        }).catch(() => console.log("Alert"))
+        getImages()
     }
 
+    useEffect(fetchImages,[])
+
+    // const openImagePickerAsync = async () => {
+    //     let permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
+    //     if (permissionResult.granted === false) {
+    //         alert('Permission to access camera roll is required!');
+    //         return;
+    //     }
+    //     let pickerResult = await ImagePicker.launchImageLibraryAsync();
+    //     if (pickerResult.cancelled === true) {
+    //         return;
+    //     }
+    //     return { localUri: pickerResult.uri }
+    // };
+
+    // const pick = async _ => {
+    //     openImagePickerAsync().then((image) => {
+    //         // set images
+    //         setImages((prevImg) => {
+    //             if (prevImg.column === 'right') { return { ...prevImg, column: 'left', left: [...prevImg.left, image] }}
+    //             else if ((prevImg.column === 'left' && (prevImg.left.length + prevImg.center.length + prevImg.right.length + 1) % 7 !== 2) || prevImg.column === 'center') { return { ...prevImg, column: 'right', right: [...prevImg.right, image] } }
+    //             return { ...prevImg, column: 'center', center: [...prevImg.center, image] }
+    //         })
+    //     }).catch(() => console.log("Alert"))
+    // }
+
     return (
-        <ScrollView contentContainerStyle={styles.container}>
-            <View>
-                <TouchableWithoutFeedback onPress={pick}>
-                    <View style={styles.header}>
-                        <Icon name="plus" size={24} color="black" />
-                    </View>
-                </TouchableWithoutFeedback>
+        <View contentContainerStyle={styles.container}>
+            {loading && <ActivityIndicator  size="large" color="#0000ff" />}
+            <ScrollView>
                 <View style={styles.images}>
                     <View>
                         {images.left.map((img, idx) => <ImageComponent image={img} key={idx} size={second} />)}
@@ -56,8 +85,8 @@ export default function ImagesScreen() {
                         {images.right.map((img, idx) => <ImageComponent image={img} key={idx} size={second} />)}
                     </View>
                 </View>
-            </View>
-        </ScrollView>
+            </ScrollView>
+        </View>
     )
 }
 
